9880feadd5b5e34b37d8cd9f50495bfa0e8704f7
Michal Borek <63055456+michalaborek@users.noreply.github.com>
2020-09-07 22:16:04 +0200

    webdriver path moved to SeleniumDriver class

--- selenium_handler.py
+++ selenium_handler.py
@@ -1,20 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# local webdriver directory
-webdriver_path = "C:/chromedriver_win32/chromedriver.exe"
-
 
 class SeleniumDriver:
     """This is a class for creating webdriver object"""
 
+    webdriver_path = "C:/chromedriver_win32/chromedriver.exe"
     main_url = 'https://www.metal-archives.com'
 
     def __init__(self):
         try:
             options = Options()
             options.add_argument('--headless')
-            self.driver = webdriver.Chrome(executable_path=webdriver_path, chrome_options=options)
+            self.driver = webdriver.Chrome(executable_path=self.webdriver_path, chrome_options=options)
             self.driver.get(self.main_url)
         except Exception as e:
             print(f'Error while launching Chrome Webdriver or accessing metal-archives.com: {e}')
