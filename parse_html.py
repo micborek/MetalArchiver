62d2f31c8964aa93f4ae0889486147375e7ea4b6
Michal Borek <63055456+michalaborek@users.noreply.github.com>
2020-09-07 22:16:55 +0200

    getting releases links from band page

--- parse_html.py
+++ parse_html.py
@@ -1,4 +1,5 @@
 # This module parses sites with BeautifulSoup
+# TODO: create a 'parser' class
 
 from bs4 import BeautifulSoup
 
@@ -25,14 +26,14 @@ def parse_search_results(html: str) -> list:
 
 
 def parse_main_info(html: str) -> dict:
-    """This function gets all the basic info from the html"""
+    """This function gets the basic info from the html - header and top columns"""
 
     basic_info = {}
     print("Getting basic info about the band.")
     soup = BeautifulSoup(html, 'lxml')
 
     # get header
-    basic_info['name'] = soup.find('h1', class_='band_name').text
+    basic_info['Name'] = soup.find('h1', class_='band_name').text
 
     # get basic info from left and right column
     left = soup.find('dl', class_='float_left')
@@ -42,23 +43,43 @@ def parse_main_info(html: str) -> dict:
     for element in info_right:
         info.append(element)
 
-    # TODO: add info from columns to basic info dictionary in a loop
-
-    # TODO: get general comment for the band
+    count = 0
+    for element in info:
+        if count == 0:
+            key = element.text
+            count += 1
+        else:
+            basic_info[key] = element.text
+            count = 0
 
     return basic_info
 
 
-def parse_band_page(html: str):
-    """This is the main point for the parsing of the band's page"""
+def get_releases_links(html: str) -> dict:
+    """This function gets the links for releases' info from the band's page """
 
-    main_info = parse_main_info(html)
-    print(main_info)
+    links = {}
+    print('Getting releases links.')
+    soup = BeautifulSoup(html, 'lxml')
+    discography_table = soup.find('table', class_='display discog')
+    disco_links = discography_table.find_all('a')
+    for link in disco_links:
+        if 'reviews' not in str(link['href']):
+            links[link.text] = link['href']
+
+    return links
+
 
 # this part below for testing offline
+
 # with open('test_data/search_results.html', 'r', encoding='utf8') as f:
 #     html_string = f.read()
 # parse_search_results(html_string)
+
 # with open('test_data/band_site.html', 'r', encoding='utf8') as f:
 #     html_string = f.read()
 # parse_band_page(html_string)
+
+# with open('test_data/band_site.html', 'r', encoding='utf8') as f:
+#     html_string = f.read()
+# get_releases_links(html_string)
