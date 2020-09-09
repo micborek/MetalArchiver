b49af7aae2fa606f0cd532f3e60ed987a6c6bb95
Michal Borek <63055456+michalaborek@users.noreply.github.com>
2020-09-09 21:19:47 +0200

    created HtmlParser class, parse_general_comment method added

--- parse_html.py
+++ parse_html.py
@@ -1,93 +1,99 @@
 """This module is for parsing different sub-pages with BeautifulSoup"""
-# TODO: create a 'parser' class
-
 from bs4 import BeautifulSoup
 
 
-def parse_search_results(html: str) -> list:
-    """Get search results to a list of dictionaries if more than one band
-    return list of dictionaries"""
+class HtmlParser:
 
-    results = []
-    count = 0
-    soup = BeautifulSoup(html, 'lxml')
+    def parse_search_results(self, html: str) -> list:
+        """Get search results to a list of dictionaries if more than one band
+        return list of dictionaries"""
 
-    search_results = soup.find('tbody')
-    if 'dataTables_empty' in str(search_results):
-        return results
-    for table_row in search_results.find_all('tr'):
-        table_data = table_row.find_all('td')
-        count += 1
-        result_dict = {'num': count, 'name': table_data[0].text, 'href': table_data[0].find('a', href=True)['href'],
-                       'genre': table_data[1].text, 'country': table_data[2].text}
-        results.append(result_dict)
+        results = []
+        count = 0
+        soup = BeautifulSoup(html, 'lxml')
 
-    return results
-
-
-def parse_main_info(html: str) -> dict:
-    """This function gets the basic info from the html - header and top columns"""
-
-    print("Getting basic info about the band.")
-    soup = BeautifulSoup(html, 'lxml')
-
-    # get basic info from left and right column
-    basic_info = parse_float_left_right(soup)
-
-    # get header
-    basic_info['Name'] = soup.find('h1', class_='band_name').text
-
-    return basic_info
-
-
-def parse_float_left_right(html: BeautifulSoup) -> dict:
-    """This one is for parsing floating columns which occur for band and release sites"""
-
-    data = {}
-    left = html.find('dl', class_='float_left')
-    info = left.find_all(['dd', 'dt'])
-    right = html.find('dl', class_='float_right')
-    info_right = right.find_all(['dd', 'dt'])
-    for element in info_right:
-        info.append(element)
-
-    count = 0
-    for element in info:
-        if count == 0:
-            key = element.text
+        search_results = soup.find('tbody')
+        if 'dataTables_empty' in str(search_results):
+            return results
+        for table_row in search_results.find_all('tr'):
+            table_data = table_row.find_all('td')
             count += 1
-        else:
-            data[key] = element.text.replace('\n', '')
-            count = 0
+            result_dict = {'num': count, 'name': table_data[0].text, 'href': table_data[0].find('a', href=True)['href'],
+                           'genre': table_data[1].text, 'country': table_data[2].text}
+            results.append(result_dict)
 
-    return data
+        return results
 
+    def parse_main_info(self, html: str) -> dict:
+        """This method gets the basic info from the html - header and top columns"""
 
-def get_releases_links(html: str) -> dict:
-    """This function gets the links for releases' info from the band's page """
+        print("Getting basic info about the band.")
+        soup = BeautifulSoup(html, 'lxml')
 
-    links = {}
-    print('Getting releases links.')
-    soup = BeautifulSoup(html, 'lxml')
-    discography_table = soup.find('table', class_='display discog')
-    disco_links = discography_table.find_all('a')
-    for link in disco_links:
-        if 'reviews' not in str(link['href']):
-            links[link.text] = link['href']
+        # get basic info from left and right column
+        basic_info = self.parse_float_left_right(soup)
 
-    return links
+        # get header
+        basic_info['Name'] = soup.find('h1', class_='band_name').text
 
+        return basic_info
 
-def parse_release_data(html: str) -> dict:
-    """This function gets data from the release page"""
+    def parse_float_left_right(self, html: BeautifulSoup) -> dict:
+        """This one is for parsing floating columns which occur for band and release sites"""
 
-    soup = BeautifulSoup(html, 'lxml')
-    release_data = parse_float_left_right(soup)
+        data = {}
+        left = html.find('dl', class_='float_left')
+        info = left.find_all(['dd', 'dt'])
+        right = html.find('dl', class_='float_right')
+        info_right = right.find_all(['dd', 'dt'])
+        for element in info_right:
+            info.append(element)
 
-    # get header
-    release_data['Name'] = soup.find('h1', class_='album_name').text
+        count = 0
+        for element in info:
+            if count == 0:
+                key = element.text
+                count += 1
+            else:
+                data[key] = element.text.replace('\n', '')
+                count = 0
 
-    return release_data
+        return data
+
+    def get_releases_links(self, html: str) -> dict:
+        """This method gets the links for releases' info from the band's page """
+
+        links = {}
+        print('Getting releases links.')
+        soup = BeautifulSoup(html, 'lxml')
+        discography_table = soup.find('table', class_='display discog')
+        disco_links = discography_table.find_all('a')
+        for link in disco_links:
+            if 'reviews' not in str(link['href']):
+                links[link.text] = link['href']
+
+        return links
+
+    def parse_release_data(self, html: str) -> dict:
+        """This method gets data from the release page"""
+
+        soup = BeautifulSoup(html, 'lxml')
+        release_data = self.parse_float_left_right(soup)
+
+        # get header
+        release_data['Name'] = soup.find('h1', class_='album_name').text
+
+        return release_data
+
+    def parse_general_comment(self, html:str) -> str:
+        """This method gets the general comment for a band"""
+
+        soup = BeautifulSoup(html, 'lxml')
+        comment = soup.find('div', class_='displayNone ui-dialog-content ui-widget-content')
+        if comment:
+            return comment.text
+
+        return ""
 
 
 # this part below for testing offline
